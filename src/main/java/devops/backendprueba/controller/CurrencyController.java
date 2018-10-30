67496f90bd99a97fc7a3c10aa6be2a007a406d81
Francisco <francisco.serrano372@gmail.com>
2018-10-30 00:04:44 -0300

    Adds origins URLs

--- src/main/java/devops/backendprueba/controller/CurrencyController.java
+++ src/main/java/devops/backendprueba/controller/CurrencyController.java
@@ -2,10 +2,15 @@ package devops.backendprueba.controller;
 
 import devops.backendprueba.service.CurrencyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {
+        "http://localhost:4200",
+        "http://localhost:9876"
+})
 public class CurrencyController {
 
     private final CurrencyService dolarService;
