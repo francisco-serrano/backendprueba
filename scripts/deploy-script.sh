a3bf2d03e7a0683cdfd9bf7b37de3b0b5b49cbfe
Cooper <marsancoo@gmail.com>
2019-02-14 01:58:07 -0300

    Made changes in order to connect to Heroku v3

--- scripts/deploy-script.sh
+++ scripts/deploy-script.sh
@@ -1,3 +1,4 @@
-docker push "$DOCKER_USERNAME"/backend-image:$COMMIT
-#docker push registry.heroku.com/$HEROKU_APP_NAME/web
-#heroku container:release web    
+wget -qO- https://toolbelt.heroku.com/install.sh | sh #install heroku CLI
+docker login --username _ --password=$HEROKU_API_KEY registry.heroku.com
+heroku container:push web --app $HEROKU_APP_NAME
+heroku container:release web --app $HEROKU_APP_NAME
