7c48a1d1edf50a1904704e9e2db172f42c0da1ea
Cooper <marsancoo@gmail.com>
2019-02-14 00:24:49 -0300

    Made changes in order to connect to Heroku

--- scripts/deploy-script.sh
+++ scripts/deploy-script.sh
@@ -1,3 +1,3 @@
-docker push "$DOCKER_USERNAME"/backend-image:$COMMIT;
-docker push registry.heroku.com/$HEROKU_APP_NAME/web;
-heroku container:release web release;
+docker push "$DOCKER_USERNAME"/backend-image:$COMMIT
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
+heroku container:release web
