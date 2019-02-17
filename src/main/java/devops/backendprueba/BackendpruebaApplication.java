837f1ff1f5c8417c762dc77a6df1b5a07da3962b
Cooper <marsancoo@gmail.com>
2019-02-17 19:33:07 -0300

    Some unidentified changes

--- src/main/java/devops/backendprueba/BackendpruebaApplication.java
+++ src/main/java/devops/backendprueba/BackendpruebaApplication.java
@@ -3,6 +3,7 @@ package devops.backendprueba;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BackendpruebaApplication {
 
